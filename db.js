2a1e3b32b4bc3fa1bf94cd6fc90efbf41ccfe5b3
th <truongquoch29@gmail.com>
2026-03-13 12:33:25 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./database.db")
+
+db.serialize(()=>{
+
+ db.run(`
+  CREATE TABLE IF NOT EXISTS users(
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   username TEXT UNIQUE,
+   password TEXT,
+   balance INTEGER DEFAULT 0
+  )
+ `)
+
+})
+
+module.exports = db
